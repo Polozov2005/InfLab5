3361e6285fee82e548594df0a2355f2fa29f4b7a
Polozov2005 <aleksandr.polozov.2005@gmail.com>
2024-04-17 10:51:01 +0700

    1

--- test2.py
+++ test2.py
@@ -1,4 +1,14 @@
 from tkinter import *
+from PIL import ImageTk, Image
+from pylatexenc.latex2text import LatexNodes2Text
 
-my_window = Tk()
-my_window.mainloop()
+root = Tk()
+root.geometry("1100x500")
+root['bg'] = 'grey'
+
+equation = r'$\frac{1}{2} \times 3 = \frac{3}{2}$' 
+equation_text = LatexNodes2Text().latex_to_text(equation)
+label = Label(root, text=equation_text, height=100, width=100)
+label.pack()
+
+root.mainloop()
